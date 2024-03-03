45d0496449980133a25e7bec2f1431332ab0f4d1
Piotr Pęgiel <p.a.pegiel@gmail.com>
2024-03-03 17:18:38 +0100

    Core structure

--- Item.java
+++ Item.java
@@ -1,2 +1,11 @@
 public class Item {
+    private String name;
+    private double nettoPrice;
+    private double vat;
+
+    public Item(String name, double nettoPrice, double vat) {
+        this.name = name;
+        this.nettoPrice = nettoPrice;
+        this.vat = vat;
+    }
 }
--- Receipt.java
+++ Receipt.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Receipt {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Receipt {
+    private List<Item> itemList = new ArrayList<>();
+
+    public void addItem(String name, double nettoPrice, double vat){
+        itemList.add(new Item(name, nettoPrice, vat));
+    }
+
+    public void printReceipt(){
+
+    }
 }
